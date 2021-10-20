cf6f270ce45479fdc46026731cc1b89f323bc9c9
adrianmcenturion <adrianmcenturion@gmail.com>
2021-10-19 23:55:42 -0300

    commit prueba

---
+++ src/components/botones/Botones.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Botones = () => {
+    return (
+        <div className="contenedor-numero">
+            <button className="boton-numero">1</button>
+            
+        </div>
+    )
+}
+
+export default Botones
